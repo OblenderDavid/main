c5b2648530e68a7ec375d5d5440ea84d4a59eaf2
David <david.oblender2002@gmail.com>
2023-06-08 02:44:12 -0400

    updated version main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cmath>
 int main(){
 
     int a = 5, b = 10, sum = a + b;
-    std::cout << "Сумма: " << sum << std::endl;
+    std::cout << "Сумма: " << sum << " Степень: " << pow(a,b) << std::endl;
 
     return 0;
 }
